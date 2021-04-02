9efbac75456e1498218122d65c36e703de45ca0a
Deo Prasad Shaw <deoshaw3@gmail.com>
2021-04-02 11:04:32 +0530

    Fixed alert test

--- Selenium_POM_PageFactory/src/test/java/TestCases/AlertTest.java
+++ Selenium_POM_PageFactory/src/test/java/TestCases/AlertTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import Base.Baseclass;
 import Pages.Guru99Alert;
+import utilities.seleniumUtilities;
 
 public class AlertTest extends Baseclass {
 	
@@ -12,6 +13,8 @@ public class AlertTest extends Baseclass {
 	@Test
 	public void CheckAlertFunctions() throws Exception
 	{
+		
+		seleniumUtilities.LaunchBrowser("http://demo.guru99.com/test/delete_customer.php",driver);
 		Guru99Alert ob=new Guru99Alert(driver,test);
 		ob.AlertFunctionTest();
 	}
