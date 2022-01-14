ae5214927ee58aca87616a6a899086a3543cd405
Deo Prasad Shaw <deoshaw3@gmail.com>
2022-01-14 10:35:47 +0530

    Added github profile pic upload test

--- Selenium_POM_PageFactory/src/main/java/Pages/GitHub.java
+++ Selenium_POM_PageFactory/src/main/java/Pages/GitHub.java
@@ -1,10 +1,14 @@
 package Pages;
 
+import java.io.File;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -45,7 +49,35 @@ public class GitHub {
 	WebElement yourRepositories;
 	@FindBy(xpath="(//*[contains(@data-hydro-click,'TAB_REPOSITORIES')]/child::span)[1]")
 	WebElement Repositories;
+	@FindBy(xpath="//*[@class='position-absolute color-bg-default rounded-2 color-fg-default px-2 py-1 left-0 bottom-0 ml-2 mb-2 border']")
+	WebElement editButton;
+	@FindAll({@FindBy(xpath="//*[@type='file']"),
+	        @FindBy(className ="manual-file-chooser width-full ml-0")})
+	WebElement uploadLink;
+	@FindBy(xpath="//*[normalize-space(text())='Set new profile picture']")
+	WebElement setProfilePicture;
 	
+	
+	void uploadProfileImage(String Message) throws Exception
+	{
+		test.log(LogStatus.PASS, "Test started");
+		//signInLink.click();
+		//wait.until(ExpectedConditions.visibilityOf(Username));
+		js.executeScript("arguments[0].value='deoshaw3@gmail.com';", Username);
+		Thread.sleep(1500);
+		js.executeScript("arguments[0].value='Devanshi@03';", Password);
+		js.executeScript("arguments[0].click();", Login);
+		test.log(LogStatus.PASS, "Login Successful");
+		//Thread.sleep(100000);
+		//editButton.click();
+		uploadLink.sendKeys(System.getProperty("user.dir")+File.separator+"Profile_Image.jpeg");
+		setProfilePicture.click();
+		js.executeScript("alert(arguments[0])", Message);
+		Thread.sleep(3000);
+		test.log(LogStatus.PASS, "Image uploaded");
+	    GitHub_Driver.switchTo().alert().accept();
+		test.log(LogStatus.PASS, "Test ended");
+	}
 	
 	public void getRepositoryCount() throws InterruptedException
 	{
--- Selenium_POM_PageFactory/src/test/java/TestCases/GitHubTest.java
+++ Selenium_POM_PageFactory/src/test/java/TestCases/GitHubTest.java
@@ -1,6 +1,11 @@
 package TestCases;
 
+import java.lang.reflect.Method;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentTest;
 
 import Base.Baseclass;
 import Pages.GitHub;
@@ -16,6 +21,18 @@ public class GitHubTest extends Baseclass {
 		seleniumUtilities.LaunchBrowser("https://github.com/",driver);
 		GitHub profile=new GitHub(driver,test);
 		profile.getRepositoryCount();
+		
+		
+	}
+	
+	@Test(priority=-1)
+	void uploadImage() throws Exception
+	{
+		seleniumUtilities.LaunchBrowser("https://github.com/settings/profile", driver);
+		GitHub ob=GitHub.class.getDeclaredConstructor(WebDriver.class,ExtentTest.class).newInstance(driver,test);
+		Method m=GitHub.class.getDeclaredMethod("uploadProfileImage",String.class);
+		m.setAccessible(true);
+		m.invoke(ob,seleniumUtilities.getDataForKey("UPLOAD_MESSAGE"));
 		
 		
 	}
