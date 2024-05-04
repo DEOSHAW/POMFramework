c4cc558003f2c110be363a83e881026441534634
Deo Prasad Shaw <deoshaw3@gmail.com>
2024-05-04 09:52:03 +0530

    Added Cosmo code test

--- Selenium_POM_PageFactory/src/main/java/Pages/CosmoCode.java
+++ Selenium_POM_PageFactory/src/main/java/Pages/CosmoCode.java
@@ -3,76 +3,32 @@ package Pages;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
-
-public class CosmoCode {
+public class CosmoCode
+{
 	WebDriver driver;
-	ExtentTest test;
-	
-	public CosmoCode(WebDriver driver,ExtentTest test)
+	public CosmoCode(WebDriver driver)
 	{
-		
 		this.driver=driver;
-		this.test=test;
 		PageFactory.initElements(driver, this);
 		
 	}
 	
-	@FindBy(how=How.XPATH,using="//table[@id='countries']//tr")
-	List<WebElement> rowList;
+	@FindBy(xpath="//table//tr")
+	List<WebElement> allRecords;
 	
-	@FindBy(xpath="//table[@id='countries']//tr[1]//td")
-	List<WebElement> columnList;
-	
-	
-	void selectCountry() throws InterruptedException
+	List<String> getRecordsFromWebTable()
 	{
-		
-		test.log(LogStatus.PASS, "Test Started");
-		
-		ArrayList<String> countries=new ArrayList<String>();
-		countries.add("India");
-		countries.add("Argentina");
-		countries.add("Japan");
-		
-		
-		for(String country:countries)
+		List<String> allData=new ArrayList<>();
+		for(WebElement eachRecord:allRecords)
 		{
-		
-		for(int i=1;i<rowList.size();i++)
-		  {
-			
-				String countryName=driver.findElement(By.xpath("//table[@id='countries']//tr["+i+"]//td[2]")).getText();
-				
-				
-				
-				
-				if(countryName.equalsIgnoreCase(country))
-				{
-					WebElement checkBox=driver.findElement(By.xpath("//table[@id='countries']//tr["+i+"]//td[1]/input"));
-					JavascriptExecutor js=(JavascriptExecutor)driver;
-					js.executeScript("arguments[0].scrollIntoView();", checkBox);
-					Thread.sleep(1500);
-					checkBox.click();
-					test.log(LogStatus.PASS, "Clicked on checkbox corresponding to country "+countryName);
-					Thread.sleep(2000);
-				}
-				
-		  }
+			allData.add(eachRecord.getText());
 		}
-		
-		test.log(LogStatus.PASS, "Test Ended");
-		
-		
+		return allData;
 		
 	}
 
--- Selenium_POM_PageFactory/src/test/java/TestCases/CosmoCodeTest.java
+++ Selenium_POM_PageFactory/src/test/java/TestCases/CosmoCodeTest.java
@@ -1,29 +1,44 @@
 package TestCases;
 
 import java.lang.reflect.Method;
+import java.util.List;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 import Base.Baseclass;
 import Pages.CosmoCode;
 import utilities.seleniumUtilities;
 
-public class CosmoCodeTest extends Baseclass {
-	
-	
+public class CosmoCodeTest extends Baseclass
+{
 	
 	@Test
-	void selectCountryOnCosmoCodePortal() throws Exception
+	void getRecordsFromCosmoCodeWebTable() throws Exception
 	{
+		test.log(LogStatus.PASS, "Test Started");
 		seleniumUtilities.LaunchBrowser("https://cosmocode.io/automation-practice-webtable/", driver);
-		
-		CosmoCode ob=CosmoCode.class.getDeclaredConstructor(WebDriver.class,ExtentTest.class).newInstance(driver,test);
-		Method m=CosmoCode.class.getDeclaredMethod("selectCountry");
+		CosmoCode ob=CosmoCode.class.getDeclaredConstructor(WebDriver.class).newInstance(driver);
+		Method m=CosmoCode.class.getDeclaredMethod("getRecordsFromWebTable");
 		m.setAccessible(true);
-		m.invoke(ob);
+		List<String> allRecords=(List<String>) m.invoke(ob);
+		test.log(LogStatus.PASS, "Retrieved All records");
+		for(String eachRecord:allRecords)
+		{
+			if(eachRecord.contains("India"))
+			{
+				Assert.assertTrue(eachRecord.contains("New Delhi"));
+			}
+		}
+		test.log(LogStatus.PASS, "Table contains New Delhi");
+		test.log(LogStatus.PASS, "Test Ended");
+		
+		
+		
+		
 	}
 
 }
